1f410dedeb9d6c8f555193dc1b6974f774c1f38e
Lucas Sousa <37560590+Lucas96Sousa@users.noreply.github.com>
2020-03-25 20:53:12 -0300

    Backend Finalizado

--- server/src/routes.js
+++ server/src/routes.js
@@ -1,9 +1,25 @@
 import { Router } from "express";
 
-import OngController from "./controller/OngsController";
+import OngController from "./controller/OngController";
+import IncidentController from "./controller/IncidentController";
+import ProfileController from "./controller/ProfileController";
+import SessionController from "./controller/SessionController";
+
 const routes = Router();
 
+//Login
+routes.post("/sessions", SessionController.create);
+
+//Ong
 routes.get("/ongs", OngController.index);
 routes.post("/ongs", OngController.store);
+
+//Incidents
+routes.get("/incidents", IncidentController.index);
+routes.post("/incidents", IncidentController.create);
+routes.delete("/incidents/:id", IncidentController.delete);
+
+//Profile
+routes.get("/profile", ProfileController.index);
 
 export default routes;
